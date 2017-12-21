8e988c00c1d575d47ae9568719328165b73e3426
Your Name <sjonesmusician@gmail.com>
2017-12-21 11:31:26 -0500

    messages appearing.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 app.get('/', (req,res)=>{
   res.sendFile(__dirname + '/index.html');
